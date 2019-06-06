193208839c43af13badb7ea38a0db0585b560d32
Shoham Jacobsen <shoham.jacobsen@gmail.com>
2019-06-06 15:04:32 +0300

    Hot-reloading setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
+//Packeges needed for express
 import express from 'express'
 import path from 'path'
-let app = express()
 
-app.use(express.static('public'))
+//Packeges needed for webpack
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config.dev.js'
 
+let app = express();
+
+//Using webpack as middleware to bundle all js
+const compiler = webpack(webpackConfig);
+app.use(webpackMiddleware(compiler));
+app.use(webpackHotMiddleware(compiler,{
+    hot: true, 
+    publicPath: webpackConfig.output.publicPath,
+    noInfo: true
+}));
+
+//Express server
 app.get("/*",  (req,res) =>{
     res.sendFile(path.join(__dirname,'./index.html'))
 })
