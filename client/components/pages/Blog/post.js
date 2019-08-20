7aacb9353304f19cec3be2879305f42b382be08f
Shoham Jacobsen <shoham.jacobsen@gmail.com>
2019-08-20 18:41:10 +0300

    Fixed blog

--- client/components/pages/Blog/post.js
+++ client/components/pages/Blog/post.js
@@ -10,10 +10,10 @@ export default class Post extends React.Component {
         return (
                 <div className="p-b-63">
                   <a href="blog-detail.html" className="hov-img0 how-pos5-parent">
-                    <img src="/images/blog/blog-04.jpg" width="200px" alt="IMG-BLOG" />
+                    <img src={image} width="200px" alt="IMG-BLOG" />
                     <div className="flex-col-c-m size-123 bg9 how-pos5">
                       <span className="ltext-107 cl2 txt-center">
-                        {date.getDate()}
+                        {22}
                       </span>
                       <span className="stext-109 cl3 txt-center">
                         Jan 2018
@@ -23,7 +23,7 @@ export default class Post extends React.Component {
                   <div className="p-t-32">
                     <h4 className="p-b-15">
                       <a href="blog-detail.html" className="ltext-108 cl2 hov-cl1 trans-04">
-                        8 Inspiring Ways to Wear Dresses in the Winter
+                        {title}
                       </a>
                     </h4>
                     <p className="stext-117 cl6">
@@ -32,15 +32,12 @@ export default class Post extends React.Component {
                     <div className="flex-w flex-sb-m p-t-18">
                       <span className="flex-w flex-m stext-111 cl2 p-r-30 m-tb-10">
                         <span>
-                          <span className="cl4">By</span> Admin  
+                          <span className="cl4">By</span> {author}
                           <span className="cl12 m-l-4 m-r-6">|</span>
                         </span>
                         <span>
-                          StreetStyle, Fashion, Couple  
+                          {categories.toString()}
                           <span className="cl12 m-l-4 m-r-6">|</span>
-                        </span>
-                        <span>
-                          8 Comments
                         </span>
                       </span>
                       <a href="blog-detail.html" className="stext-101 cl2 hov-cl1 trans-04 m-tb-10">
